6afcb4ea4ec836e1f8fed4dd223d8af3c6dfc816
amevide998 <hdsvidi@gmail.com>
2023-11-07 15:53:06 +0700

    add cardlist

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "@/components/pagination/Pagination";
+import Image from "next/image";
+import {Card} from "@/components/card/Card";
 
 
 export default function CardList() {
     return (
         <div className={styles.container}>
-            cardlist
+            <h1 className={styles.title}>
+                Recent Post
+            </h1>
+            <div className={styles.posts}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
             <Pagination />
         </div>
     );
