69b89f55ea846c2d4a4d7a371a11b0559b23ae0e
amevide998 <hdsvidi@gmail.com>
2023-11-07 16:58:00 +0700

    add menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,209 @@
 import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 
 export default function Menu() {
     return (
         <div className={styles.container}>
-            <h1>Menu</h1>
+            <h2 className={styles.subTitle}>What's Hot</h2>
+            <h1 className={styles.title}>Most Popular</h1>
+            <div className={styles.items}>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.travel}`}>
+                            Travel
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.culture}`}>
+                            Culture
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.food}`}>
+                            Food
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.travel}`}>
+                            Travel
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+            <h2 className={styles.subTitle}>Discover by Topic</h2>
+            <h1 className={styles.title}>Categories</h1>
+            <div className={styles.categoryList}>
+                <Link href={"/blog?cat=style"} className={`${styles.categoryItem} ${styles.style}`}>
+                    Style
+                </Link>
+                <Link href={"/blog?cat=style"} className={`${styles.categoryItem} ${styles.culture}`}>
+                    Culture
+                </Link>
+                <Link href={"/blog?cat=style"} className={`${styles.categoryItem} ${styles.food}`}>
+                    Food
+                </Link>
+                <Link href={"/blog?cat=style"} className={`${styles.categoryItem} ${styles.coding}`}>
+                    Coding
+                </Link>
+                <Link href={"/blog?cat=style"} className={`${styles.categoryItem} ${styles.fashion}`}>
+                    Fashion
+                </Link>
+                <Link href={"/blog?cat=style"} className={`${styles.categoryItem} ${styles.travel}`}>
+                    Travel
+                </Link>
+            </div>
+
+
+            <h2 className={styles.subTitle}>Choose by editor</h2>
+            <h1 className={styles.title}>editor pick</h1>
+            <div className={styles.items}>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image src={'/p1.jpeg'} alt={""} className={styles.image} fill />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.travel}`}>
+                            Travel
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image src={'/p1.jpeg'} alt={""} className={styles.image} fill />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.culture}`}>
+                            Culture
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image src={'/p1.jpeg'} alt={""} className={styles.image} fill />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.food}`}>
+                            Food
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image src={'/p1.jpeg'} alt={""} className={styles.image} fill />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.travel}`}>
+                            Travel
+                        </span>
+                        <h3 className={styles.postTitle}>
+                            some random text from heaven
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>
+                                Michael Corleone
+                            </span>
+                            {" - "}
+                            <span className={styles.date}>
+                                11.02.2023
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
