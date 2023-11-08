7a005a11de2428f0d48d4a2c0fd6cf9434f70bab
amevide998 <hdsvidi@gmail.com>
2023-11-08 15:52:05 +0700

    add prisma adapter

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,10 @@
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import prisma from "@/utils/connect";
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
