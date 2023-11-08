3a652a23a89be6989425196ffd46b034b4c99b5a
amevide998 <hdsvidi@gmail.com>
2023-11-08 15:23:40 +0700

    auth layout - add auth functional

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,19 +1,38 @@
+"use client"
 import styles from './loginPage.module.css';
+import {signIn, useSession} from "next-auth/react";
+import {redirect} from "next/navigation";
 
 export default function loginPage () {
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div className={styles.socialButton}>
-                    Continue With Google
-                </div>
-                <div className={styles.socialButton}>
-                    Continue With Github
-                </div>
-                <div className={styles.socialButton}>
-                    Continue With Facebook
+    const {data, status} = useSession()
+
+    if(status === 'loading'){
+        return (
+            <div className={styles.loading}>
+                Loading ...
+            </div>
+        )
+    }
+    else if (status === 'authenticated') {
+        redirect('/')
+    }
+    else {
+        return (
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    <div className={styles.socialButton}
+                         onClick={() => signIn("google")}
+                    >
+                        Continue With Google
+                    </div>
+                    <div className={styles.socialButton}>
+                        Continue With Github
+                    </div>
+                    <div className={styles.socialButton}>
+                        Continue With Facebook
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
