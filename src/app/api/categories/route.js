17b165fe5d1d10ca44c18b73a3d69bc8355b75e7
amevide998 <hdsvidi@gmail.com>
2023-11-08 17:05:37 +0700

    add endpoint for catefories

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,24 @@
+import {NextResponse} from "next/server";
+import prisma from "@/utils/connect";
+
+export const GET = async () => {
+    try{
+        const categories = await prisma.category.findMany();
+
+        return new NextResponse(
+            JSON.stringify(categories, {
+                status : 200
+            })
+        )
+
+    }catch (err){
+        console.log("api/categories/get : ", err)
+        return new NextResponse(
+            JSON.stringify({
+                message : "Internal Server Error"
+            }, {
+                status: 500
+            })
+        )
+    }
+}
