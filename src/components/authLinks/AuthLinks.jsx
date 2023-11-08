242217e10ebd0111333508605c3b71d3103c544e
amevide998 <hdsvidi@gmail.com>
2023-11-08 15:51:29 +0700

    change status using useSession

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,11 +2,11 @@
 import styles from "./authLinks.module.css";
 import Link from "next/link";
 import {useState} from "react";
-import {signOut} from "next-auth/react";
+import {signOut, useSession} from "next-auth/react";
 
 export default AuthLinks => {
 
-    const status = "notauthenticated"
+    const status = useSession()
 
     const [open, setOpen] = useState(false);
 
@@ -38,7 +38,7 @@ export default AuthLinks => {
                         <Link href={"/"}>Contact</Link>
                         <Link href={"/"}>About</Link>
                         {
-                            status === "notauthenticated" ?
+                            status === "unauthenticated" ?
                                 (
                                     <Link href={"/auth/login"}>Login</Link>
                                 ):
