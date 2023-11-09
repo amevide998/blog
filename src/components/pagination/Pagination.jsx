44a4448ad070074a711cd6f1d7e8bd46d0c59b45
amevide998 <hdsvidi@gmail.com>
2023-11-09 16:32:37 +0700

    update - using api

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,12 +1,17 @@
+"use client"
 import React from "react";
 import styles from "./pagination.module.css";
+import {useRouter} from "next/navigation";
 
 
-export default function Pagination() {
+export default function Pagination({page, hasPrev, hasNext}) {
+
+    const router = useRouter();
+
     return (
         <div className={styles.container}>
-            <button className={styles.button}>Previous</button>
-            <button className={styles.button}>Next</button>
+            <button className={styles.button} onClick={()=> router.push(`/?page=${page-1}`, {scroll: true})} disabled={!hasPrev} >Previous</button>
+            <button className={styles.button} onClick={()=> router.push(`/?page=${page+1}`, {scroll: true})} disabled={!hasNext} >Next</button>
 
         </div>
     );
