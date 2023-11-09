9e8fee4015e192fe2bc0ee26b120c7dcc93858ec
amevide998 <hdsvidi@gmail.com>
2023-11-09 16:29:34 +0700

    post api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,11 +1,12 @@
 import {NextResponse} from "next/server";
 import prisma from "@/utils/connect";
+import {getAuthSession} from "@/utils/auth";
 
 export const GET = async (req) => {
 
-    const {searchParam} = new URL(req.url);
-
-    const page = searchParam.page;
+    const {searchParams} = new URL(req.url);
+    const page = searchParams.get('page');
+    const category = searchParams.get("category")
 
     if(!page){
         return new NextResponse(
@@ -20,18 +21,29 @@ export const GET = async (req) => {
     const POST_PER_PAGE = 2
 
     try{
-        const categories = await prisma.category.findMany(
-            {
-                take: POST_PER_PAGE,
-                skip: POST_PER_PAGE * (page - 1),
-                orderBy: {
-                    createdAt: "desc"
-                }
-            }
+        const [posts, count] = await prisma.$transaction(
+            [
+                prisma.post.findMany(
+                    {
+                        take: POST_PER_PAGE,
+                        skip: POST_PER_PAGE * (page - 1),
+                        orderBy: {
+                            createdAt: 'desc'
+                        },
+                        where: {
+                            ...(category && {
+                                catSlug: category
+                            })
+                        }
+                    }
+                ),
+                prisma.post.count()
+            ]
+
         );
 
         return new NextResponse(
-            JSON.stringify(categories, {
+            JSON.stringify({posts, count}, {
                 status : 200
             })
         )
@@ -40,6 +52,41 @@ export const GET = async (req) => {
         return new NextResponse(
             JSON.stringify({
                 message : "Internal Server Error"
+            }, {
+                status: 500
+            })
+        )
+    }
+}
+
+
+
+export const POST = async (req) => {
+
+    const session = await getAuthSession();
+
+    if(!session){
+        return new NextResponse(
+            JSON.stringify({message: "not authenticated"}, {status: 401})
+        )
+    }
+
+    try{
+        const body = await req.json();
+        const post = await prisma.post.create({
+            data: {...body, userEmail: session.user.email}
+        })
+
+        return new NextResponse(
+            JSON.stringify(post, {
+                status : 200
+            })
+        )
+    }catch (err){
+        console.log("api/categories/get : ", err)
+        return new NextResponse(
+            JSON.stringify({
+                message : `Internal Server Error`
             }, {
                 status: 500
             })
