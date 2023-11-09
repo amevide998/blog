0e228a4a518b2f7c58c3aaebe97d7e7f52a6a33b
amevide998 <hdsvidi@gmail.com>
2023-11-09 16:31:20 +0700

    configuration domain

--- next.config.js
+++ next.config.js
@@ -6,6 +6,22 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/a/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
