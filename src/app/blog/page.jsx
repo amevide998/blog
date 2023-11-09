7955d805edc2581c6c0ac9e2d40146047eec3d50
amevide998 <hdsvidi@gmail.com>
2023-11-09 16:32:07 +0700

    update - using api

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,14 +3,17 @@ import styles from "./blogPage.module.css";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
 
-export default function BlogPage (){
+export default function BlogPage ({searchParams}){
+    const page = parseInt(searchParams.page) || 1;
+    const category = searchParams.category || "";
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>
-                Style Blog
+                {category}
             </h1>
             <div className={styles.content}>
-                <CardList />
+                <CardList page={page} category={category}/>
                 <Menu />
             </div>
         </div>
