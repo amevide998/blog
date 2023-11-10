b0e4e7fe176f64e49ca48de4bb356a918931d80a
amevide998 <hdsvidi@gmail.com>
2023-11-10 21:25:31 +0700

    change prisma adapter

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,11 @@
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import {PrismaAdapter} from "@auth/prisma-adapter";
-import prisma from "@/utils/connect";
+// import prisma from "@/utils/connect";
 import {getServerSession} from "next-auth";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
